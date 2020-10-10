2aba6c945accb2f1483e84e81b0503abb645bf5b
MiruTsai <miru.tsai@thinkpower-info.com>
2020-10-10 21:45:17 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <v-navbar />
+    <router-view/>
+    <v-footer />
+  </div>
+</template>
+
+<script>
+import navBar from './components/NavBar'
+import footer from './components/Footer'
+
+export default {
+  components: {
+    'v-navbar': navBar,
+    'v-footer': footer
+  },
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  margin: 0 auto;
+  text-decoration: none;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,77 @@
+<template>
+  <b-container fluid class="footer">
+    <b-row>
+      <b-col cols="6">
+        <img src="" alt="" class="logo">
+        <p>碩美牙醫擁有經驗豐富的台大專業醫師團隊及頂尖的數位醫療設備，在這裡您將感受親切服務誠摯溝通、醫師的縝密診斷與適切治療為您把關口腔健康，讓您重拾自信的微笑。</p>
+        <div class="copyRight">
+          Copyright 2020  © 碩美牙醫診所
+        </div>
+      </b-col>
+      <b-col cols="3">
+        <div class="title">
+          SITE MAP
+        </div>
+        <ul class="mapList">
+          <li class="site">關於碩美</li>
+          <li class="site">診所資訊</li>
+          <li class="site">診療項目</li>
+          <li class="site">聯絡我們</li>
+          <li class="site">案例分享</li>
+          <li class="site">隱私權政策</li>
+          <li class="site">牙醫百顆</li>
+          <li class="site">服務條款</li>
+        </ul>
+      </b-col>
+      <b-col cols="3">
+        <div class="title">CONTACT US</div>
+        <div class="contactIcon">
+          <img src="" alt="">
+          <img src="" alt="">
+        </div>
+        <ul>
+          <li><img src="" alt=""><span class="info">Charmingclinic@gmail.com</span></li>
+          <li><img src="" alt=""><span class="info">(02) 8369-2796</span></li>
+          <li><img src="" alt=""><span class="info">台北市建國南路二段276號9樓</span></li>
+        </ul>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style scoped>
+.footer {
+  background-color:  #bfb7ac;
+  color: #fff;
+  height: 365.4px;
+  margin: 0 auto;
+  position: absolute;
+  bottom: 0;
+}
+.title {
+  padding: 10px 0;
+  border-bottom: 1px solid #fff;
+}
+.mapList {
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+}
+.mapList li {
+  flex-basis: 50%;
+}
+.info {
+  font-size: 14px;
+  font-weight: 300;
+  line-height: 1.43;
+  letter-spacing: 2.8px;
+}
+</style>
