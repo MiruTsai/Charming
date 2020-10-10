8b941135077baf88bb70353e3e5ffb89ee0a4502
MiruTsai <miru.tsai@thinkpower-info.com>
2020-10-10 21:59:35 +0800

    first commit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -21,7 +21,6 @@
           <li class="site">隱私權政策</li>
           <li class="site">牙醫百顆</li>
           <li class="site">服務條款</li>
-          <li class="site"></li>
         </ul>
       </b-col>
       <b-col cols="3">
